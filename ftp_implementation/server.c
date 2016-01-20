b73683163d7444cd8c6299e4a2fc684fc99f54c4
Mayank <kumar.mayank98@gmail.com>
2016-01-20 16:41:01 +0530

    Baic Server Client Implementation Added

---
+++ ftp_implementation/server.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#define PORT "3490" 
+#define BACKLOG 10  
+void sigchld_handler(int s)
+{
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) 
+	{
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+int main(void)
+{
+	int sockfd, new_fd; 
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; 
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) 
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	for(p = servinfo; p != NULL; p = p->ai_next) 
+	{
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol)) == -1) 
+		{
+			perror("server: socket");
+			continue;
+		}
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,sizeof(int)) == -1) 
+		{
+			perror("setsockopt");
+			exit(1);
+		}
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) 
+		{
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+		break;
+	}
+	if (p == NULL) 
+	{
+		fprintf(stderr, "server: failed to bind\n");
+		return 2;
+	}
+	freeaddrinfo(servinfo); 
+	if (listen(sockfd, BACKLOG) == -1) 
+	{
+		perror("listen");
+		exit(1);
+	}
+	sa.sa_handler = sigchld_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) 
+	{
+		perror("sigaction");
+		exit(1);
+	}
+	char buf[128];
+	int numbytes;
+	printf("server: waiting for connections...\n");
+	while(1) 
+	{ 
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) 
+		{
+			perror("accept");
+			continue;
+		}
+		inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr *)&their_addr),s, sizeof s);
+		printf("server: got connection from %s\n", s);
+		if (!fork()) 
+		{
+		 // this is the child process
+			close(sockfd); // child doesn't need the listener
+			if (send(new_fd, "Hello, world!", 13, 0) == -1)
+				perror("send");
+			if ((numbytes = recv(new_fd, buf, sizeof(buf), 0)) == -1) 
+			{
+				perror("recv");
+				exit(1);
+			}
+			buf[numbytes] = '\0';
+			printf("I recieved %s\n",buf);
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd); // parent doesn't need this
+	}
+	return 0;
+}
---
+++ ftp_implementation/tester.py
@@ -0,0 +1,6 @@
+import subprocess
+import sys
+#subprocess.call("")
+for i in range(26):
+	subprocess.call(["./client","localhost","&"]);
+
